8896db1b292078333584bb4a50f0b7078c757baa
Jason Dreisbach <jtdreisb@zephyr.local>
2011-03-02 18:59:56 -0800

    hbridge driver code

---
+++ hbridge.c
@@ -0,0 +1,188 @@
+
+
+//! Specifies the valid values that can be passed to the hbridgeDirection() function.
+typedef enum
+{
+	LEFT,
+	RIGHT,
+	FORWARD,
+	BACKWARD,
+	FBBRAKE,
+	LRBRAKE,
+	STOP
+} HBridgeDirection;
+
+typedef enum
+{
+	F = 3,
+	B = 2,
+	L = 7,
+	R = 6
+} HBridge;
+
+typedef enum
+{
+	M0 = 9,
+	M1 = 8,
+	M2 = 5,
+	M3 = 4,
+	Front = 0,
+	Back = 1,
+	Left = 2,
+	Right = 3
+} HMotor;
+
+u08 counter = 0;
+u08 motor[4] = {50, 50, 50, 50};
+
+void hbridgeInit()
+{
+	// initialize stuff
+	
+	//Set up Timer 5 in 8-bit fast PWM mode with the prescaler at I/O clock / 64,
+	//with the PWM in normal polarity (meaning motor on or brake on is logic 1).
+	//TCCR5B = (1 << CS50);
+	TCCR5A = (1 << WGM50);
+	TCCR5B = (1 << CS51) | (1 << WGM52);
+
+	//enable interrupt for output compare unit 3C
+	TIMSK5 |= _BV(OCIE5C);
+	
+	digitalDirection(M0, OUTPUT);
+	digitalDirection(M1, OUTPUT);
+	digitalDirection(M2, OUTPUT);
+	digitalDirection(M3, OUTPUT);
+	digitalDirection(L, OUTPUT);
+	digitalDirection(R, OUTPUT);
+	digitalDirection(B, OUTPUT);
+	digitalDirection(F, OUTPUT);
+}
+
+void hbridgeOutput(HBridge num, u08 value)
+{
+	//if the output should be set
+	if (value > 0)
+	{
+		sbi(PORTA, num - 2);
+	}
+	//else the output should be cleared
+	else
+	{
+		cbi(PORTA, num - 2);
+	}
+}
+
+// sets the speed
+// speed is between 0 and 100
+// 0 is coast
+// 100 is full speed
+void hbridgeSpeed(HBridge num, s08 speed)
+{
+	switch (num)
+	{
+		case F:
+			motor[0] = speed;
+			break;
+			
+		case B:
+			motor[1] = speed;
+			break;
+			
+		case L:
+			motor[2] = speed;
+			break;
+			
+		case R:
+			motor[3] = speed;
+			break;
+	}
+}
+
+// sets the direction of movement
+void hbridgeDirection(HBridgeDirection direction)
+{
+	switch (direction)
+	{
+		case LEFT:
+			hbridgeOutput(L, 1);
+			hbridgeOutput(R, 0);
+			break;
+			
+		case RIGHT:
+			hbridgeOutput(R, 1);
+			hbridgeOutput(L, 0);
+			break;
+			
+		case FORWARD:
+			hbridgeOutput(F, 1);
+			hbridgeOutput(B, 0);
+			break;
+			
+		case BACKWARD:
+			hbridgeOutput(B, 1);
+			hbridgeOutput(F, 0);
+			break;
+			
+		case FBBRAKE:
+			hbridgeOutput(F, 0);
+			hbridgeOutput(B, 0);
+			break;
+			
+		case LRBRAKE:
+			hbridgeOutput(L, 0);
+			hbridgeOutput(R, 0);
+			break;
+			
+		case STOP:
+			hbridgeOutput(F, 0);
+			hbridgeOutput(B, 0);
+			hbridgeOutput(L, 0);
+			hbridgeOutput(R, 0);
+			break;
+	}
+}
+
+ISR(TIMER5_COMPC_vect)
+{
+	counter++;
+	// if (counter > 50)
+	// {
+		// clearScreen();
+		// printString("ISR2");
+	// }
+	// else
+	// {
+		// clearScreen();
+		// printString("ISR1");
+	// }
+	
+	if (counter == 100)
+	{
+		counter = 0;
+		hbridgeOutput(M0, 1);
+		// hbridgeOutput(M0, 1);
+		// hbridgeOutput(M1, 1);
+		// hbridgeOutput(M2, 1);
+		// hbridgeOutput(M3, 1);
+	}
+	
+	if (motor[0] == counter)
+	{
+		hbridgeOutput(M0, 0);
+	}
+	
+	// if (motor[1] == counter)
+	// {
+		// hbridgeOutput(M1, 0);
+	// }
+	
+	// if (motor[2] == counter)
+	// {
+		// hbridgeOutput(M2, 0);
+	// }
+	
+	// if (motor[3] == counter)
+	// {
+		// hbridgeOutput(M3, 0);
+	// }
+}
--- main.c
+++ main.c
@@ -1,11 +1,88 @@
-//MyProjectName
-//Author, Date
 
-#include <globals.h>
+#include "XiphosLibrary/globals.h"
+#include "hbridge.c"
+#include <util/delay.h>
+
+#define DELAY 10
+#define SHOOTER_SPEED 255
+#define COLLECTOR_SPEED 160
+
+void moveX(s08 speed);
+void moveY(s08 speed);
 
 int main()
 {
 	initialize();
+	servoInit();
+	hbridgeInit();
 
-	printString("Hello World!");
+	//Test 1
+	clearScreen();
+	printString("   Xiphos 1.0   ");
+	lowerLine();
+	printString(" RoboAddict.com");
+	buttonWait();
+
+	
+	clearScreen();
+	printString("__Multi-Plexi___");
+	
+	moveX(80);
+	moveY(10);
+}
+
+void turnOnCollector()
+{
+	motor0(SHOOTER_SPEED);
+	motor1(COLLECTOR_SPEED);
+}
+
+void turnOffCollector()
+{
+	motor0(SHOOTER_SPEED);
+	motor1(COLLECTOR_SPEED);
+}
+
+// speed is between -100 and 100
+void moveX(s08 speed)
+{
+	if (speed > 0)
+	{
+		hbridgeDirection(RIGHT);
+		hbridgeSpeed(L, speed);
+		hbridgeSpeed(R, speed);
+	}
+	else if (speed < 0)
+	{
+		hbridgeDirection(LEFT);
+		hbridgeSpeed(L, speed&~0x80);
+		hbridgeSpeed(R, speed&~0x80);
+	}
+	else
+	{
+		hbridgeSpeed(L, 0);
+		hbridgeSpeed(R, 0);
+	}
+}
+
+// speed is between -100 and 100
+void moveY(s08 speed)
+{
+	if (speed > 0)
+	{
+		hbridgeDirection(FORWARD);
+		hbridgeSpeed(F, speed);
+		hbridgeSpeed(B, speed);
+	}
+	else if (speed < 0)
+	{
+		hbridgeDirection(BACKWARD);
+		hbridgeSpeed(F, speed&~0x80);
+		hbridgeSpeed(B, speed&~0x80);
+	}
+	else
+	{
+		hbridgeSpeed(F, 0);
+		hbridgeSpeed(B, 0);
+	}
 }
