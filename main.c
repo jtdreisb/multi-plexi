cb3eaff6350520a45953c865acd3eb4ee2c846d1
Stephanie Krapil <stkkra@gmail.com>
2011-04-12 21:20:07 -0700

    added code to separate red bot from blue bot, added function to move red bot to right wall

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #define DELAY 10
 #define SHOOTER_SPEED 80
 #define COLLECTOR_SPEED 105
+//CHANGE AS NEEDED BEFORE DOWNLOADING TO BOT!
+#define RB 1 //0 is blue, 1 is red
 
 void collectBackwards();
 void turnOnCollector();
@@ -17,42 +19,61 @@ void goForwards();
 int main()
 {
 	initialize();
-	servoRange(0, 11);
-	
-	for (;;) {
-		
-		servo(0, 0);
-		buttonWait();
-		servo(0, 255);	
-		buttonWait();	
-
-	}
-	
-	
 	hbridgeInit();
 
 	moveX(0);
 	moveY(0);
-	//Test 1
-	clearScreen();
-	printString("Test Move");
-	lowerLine();
 	
+	if(RB == 0) { 
+		//Test 1
+		clearScreen();
+		printString("Blue Bot!");
+		lowerLine(); 
+		printString("Test Move");
+		lowerLine();
+		
 
-	buttonWait();
-	clearScreen();
-	printString("collecting");	
-	
-	digitalDirection(0,INPUT);
-	digitalDirection(1, INPUT);
-	
-	while(!getButton1()) {
-		turnOnCollector();
-		collectBackwards();
-		turnOffCollector();
-		goForwards();
+		buttonWait();
+		clearScreen();
+		printString("collecting");	
+		
+		digitalDirection(0,INPUT);
+		digitalDirection(1, INPUT);
+		
+		while(!getButton1()) {
+			goForwards();
+			turnOnCollector();
+			collectBackwards();
+			turnOffCollector();
+		}
+
 	}
-	
+	else { //assuming RB = 1, red bot
+		//Test 1
+		clearScreen();
+		printString("Red Bot!");
+		lowerLine(); 
+		printString("Test Move");
+		lowerLine();
+		
+
+		buttonWait();
+		clearScreen();
+		printString("collecting");	
+		
+		digitalDirection(0,INPUT);
+		digitalDirection(1, INPUT);
+		
+		goRightWall();
+		while(!getButton1()) {
+			turnOnCollector();
+			collectBackwards();
+			turnOffCollector();
+			goForwards();
+		}
+	}
+
+    delayMs(5000);	
 	clearScreen();
 	printString("DONE");
 }
@@ -85,6 +106,20 @@ void collectBackwards()
 
 }
 
+void goRightWall() 
+{
+	moveX(20); //might be backwards/wrong speed
+	moveY(30);
+	while(!digitalInput(0)) {
+		delayMs(50);
+	}
+	moveY(0); //moveY(1) to keep up against the wall?
+	while(!digitalInput(2)) { //assuming the right side sensor is di2
+		delayMs(50);
+    }
+	moveX(0);
+}
+
 void turnOnCollector()
 {
 	motor1(SHOOTER_SPEED);
@@ -96,5 +131,3 @@ void turnOffCollector()
 	motor1(127);
 	motor0(127);
 }
-
-
