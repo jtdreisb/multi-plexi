3912adeee0597cf750ea7eeac09a59523bb4113e
Jason Dreisbach <jtdreisb@gmail.com>
2011-04-12 19:05:31 -0700

    current left side code

---
+++ pindefs.h
@@ -0,0 +1,19 @@
+
+//========================== DIGITAL PINS =================================//
+// motor power pwm ports
+
+// green/black
+#define Y_MOTOR_FRONT	9  
+#define Y_MOTOR_BACK 	8
+#define X_MOTOR_LEFT	7
+#define X_MOTOR_RIGHT	6
+
+// difference between A and B controls direction of motor
+// red/black
+#define Y_A		5
+#define Y_B		4
+#define X_A		3
+#define X_B		2
+
+
+//========================== ANALOG PINS =================================//
